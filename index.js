c851c8ef7c9964b428b6a7b126c19abfe94db932
Mykola Driuchatyi <druchaty@gmail.com>
2025-02-13 13:35:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+const app = new express();
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+    return res.send("Hello World!");
+});
+
+let server = app.listen(port, () => {
+    console.log("Listening at: http://localhost:" + port);
+});
